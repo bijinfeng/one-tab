442c9fbbd60cd73ecd8ff1ab5f0dd4bda5310c31
jfbi <jfbi@iflytek.com>
2024-08-27 10:33:28 +0800

    chore: add eslint react format config

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,47 @@
 import antfu from "@antfu/eslint-config"
+import format from "eslint-plugin-format"
 
-export default antfu({
-  stylistic: {
-    indent: 2, // 4, or 'tab'
-    quotes: "double", // or 'single'
+export default antfu(
+  {
+    stylistic: {
+      indent: 2, // 4, or 'tab'
+      quotes: "double", // or 'single'
+    },
+
+    formatters: {
+      /**
+       * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
+       * By default uses Prettier
+       */
+      css: true,
+      /**
+       * Format HTML files
+       * By default uses Prettier
+       */
+      html: true,
+      /**
+       * Format Markdown files
+       * Supports Prettier and dprint
+       * By default uses Prettier
+       */
+      markdown: "prettier",
+    },
+
+    typescript: true,
+    react: true,
+
+    ignores: ["packages/components/src/components/ui"],
   },
-
-  formatters: {
-    /**
-     * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
-     * By default uses Prettier
-     */
-    css: true,
-    /**
-     * Format HTML files
-     * By default uses Prettier
-     */
-    html: true,
-    /**
-     * Format Markdown files
-     * Supports Prettier and dprint
-     * By default uses Prettier
-     */
-    markdown: "prettier",
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: format.parserPlain,
+    },
+    plugins: {
+      format,
+    },
+    rules: {
+      "format/prettier": ["error", { parser: "typescript", tabWidth: 2 }],
+    },
   },
-
-  typescript: true,
-  react: true,
-
-  ignores: ["packages/components/src/components/ui"],
-})
+)
--- packages/web-app/src/components/IconButton.tsx
+++ packages/web-app/src/components/IconButton.tsx
@@ -1,18 +1,25 @@
-import { cn } from "@pingtou/shadcn-ui"
-import type { FC, PropsWithChildren } from "react"
+import { cn } from "@pingtou/shadcn-ui";
+import type { FC, PropsWithChildren } from "react";
 
 interface IconButtonProps extends React.HTMLAttributes<HTMLDivElement> {
-  size?: "default" | "large" | "small"
-  ghost?: boolean
+  size?: "default" | "large" | "small";
+  ghost?: boolean;
 }
 
-export const IconButton: FC<PropsWithChildren<IconButtonProps>> = ({ children, className, ...rest }) => {
+export const IconButton: FC<PropsWithChildren<IconButtonProps>> = ({
+  children,
+  className,
+  ...rest
+}) => {
   return (
     <div
       {...rest}
-      className={cn("icon-box flex h-[28px] w-[28px] cursor-pointer items-center justify-center rounded-[8px] bg-opacity-80 hover:bg-color-white hover:bg-opacity-80", className)}
+      className={cn(
+        "icon-box flex h-[28px] w-[28px] cursor-pointer items-center justify-center rounded-[8px] bg-opacity-80 hover:bg-color-white hover:bg-opacity-80",
+        className,
+      )}
     >
       {children}
     </div>
-  )
-}
+  );
+};
