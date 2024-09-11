d5cf388068aba7b9d52331d3996c8ead2c30b551
jfbi <jfbi@iflytek.com>
2024-09-11 18:25:36 +0800

    feat(ui): add select component

--- packages/ui/src/ui/select/index.tsx
+++ packages/ui/src/ui/select/index.tsx
@@ -1,21 +1,32 @@
-import { SelectContent, SelectGroup, SelectItem, SelectLabel, Select as SelectPrimitive, SelectTrigger, SelectValue } from "./select"
+import { cn } from "@/utils";
+import { CSSProperties, forwardRef } from "react";
+import { SelectContent, SelectItem, Select as SelectPrimitive, SelectTrigger, SelectValue } from "./select";
 
-export const Select = () => {
+export interface SelectProps extends Omit<React.ComponentPropsWithoutRef<typeof SelectPrimitive>, "onValueChange"> {
+  className?: string;
+  style?: CSSProperties;
+  options?: { value: string; label: React.ReactNode }[];
+  placeholder?: string;
+  side?: "top" | "right" | "bottom" | "left";
+  align?: "center" | "start" | "end";
+  onChange?: (value: string) => void
+}
+
+export type SelectRef = React.ElementRef<typeof SelectTrigger>
+
+export const Select = forwardRef<SelectRef, SelectProps>((props, ref) => {
+  const { className, style, options, placeholder, side, align, onChange, ...rest } = props;
+
   return (
-    <SelectPrimitive>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select a fruit" />
+    <SelectPrimitive {...rest} onValueChange={onChange}>
+      <SelectTrigger ref={ref} className={cn(className)} style={style}>
+        <SelectValue placeholder={placeholder} />
       </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
-        </SelectGroup>
+      <SelectContent side={side} align={align}>
+        {options?.map((item) => (
+          <SelectItem value={item.value} key={item.value}>{item.label}</SelectItem>
+        ))}
       </SelectContent>
     </SelectPrimitive>
   )
-}
+})
