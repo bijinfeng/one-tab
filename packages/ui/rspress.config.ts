850ac29359a78bfb336daffaf4f95eba84dd72ba
jfbi <jfbi@iflytek.com>
2024-09-11 11:56:08 +0800

    feat(ui): init project

---
+++ packages/ui/rspress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  // 文档根目录
+  root: 'docs',
+  title: "Rspress",
+  description: "Rspack-based Static Site Generator",
+  icon: "/rspress-icon.png",
+  logo: {
+    light: "/rspress-light-logo.png",
+    dark: "/rspress-dark-logo.png",
+  },
+  themeConfig: {
+    socialLinks: [{ icon: "github", mode: "link", content: "https://github.com/web-infra-dev/rspress" }],
+  },
+});
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,3 @@
+export * from "./ui/button"
+export * from "./utils"
+
