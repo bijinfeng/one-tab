ba70b09fc5da8e4da013b59b78b0e73c4d3c8d14
Kebai <2804673379@qq.com>
2024-09-11 23:13:21 +0800

    fix: fix eslint error

--- eslint.config.js
+++ eslint.config.js
@@ -1,47 +1,6 @@
-import antfu from "@antfu/eslint-config"
-import format from "eslint-plugin-format"
+import antfu from '@antfu/eslint-config'
 
-export default antfu(
-  {
-    stylistic: {
-      indent: 2, // 4, or 'tab'
-      quotes: "double", // or 'single'
-    },
-
-    formatters: {
-      /**
-       * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
-       * By default uses Prettier
-       */
-      css: true,
-      /**
-       * Format HTML files
-       * By default uses Prettier
-       */
-      html: true,
-      /**
-       * Format Markdown files
-       * Supports Prettier and dprint
-       * By default uses Prettier
-       */
-      markdown: "prettier",
-    },
-
-    typescript: true,
-    react: true,
-
-    ignores: ["packages/components/src/components/ui"],
-  },
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parser: format.parserPlain,
-    },
-    plugins: {
-      format,
-    },
-    rules: {
-      "format/prettier": ["error", { parser: "typescript", tabWidth: 2, printWidth: 120 }],
-    },
-  },
-)
+export default antfu({
+  formatters: true,
+  react: true,
+})
