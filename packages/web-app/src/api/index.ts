7cc174b9ce9a1b8bf317b184bdc4d2040c13f7f9
bijinfeng <bijinfeng1016@gmail.com>
2024-10-31 06:43:02 +0000

    feat(web-app): 添加获取 app 分组接口

--- packages/web-app/src/api/index.ts
+++ packages/web-app/src/api/index.ts
@@ -17,37 +17,29 @@ export async function getSearchSuggestion(keyword: string) {
 	const CancelToken = axios.CancelToken;
 	cancelSource = CancelToken.source();
 
-	const res = await axios.get<{ s: string[] }>(
-		`https://suggestion.baidu.com/su?wd=${keyword}`,
-		{
-			headers: { xxx: 1 },
-			cancelToken: cancelSource.token,
-			adapter: jsonpAdapter,
-			callbackParamName: "cb",
-		},
-	);
+	const res = await axios.get<{ s: string[] }>(`https://suggestion.baidu.com/su?wd=${keyword}`, {
+		headers: { xxx: 1 },
+		cancelToken: cancelSource.token,
+		adapter: jsonpAdapter,
+		callbackParamName: "cb",
+	});
 
 	return res.data.s;
 }
 
 export const getHitokoto = async () => {
-	const res = await axios.get<OneTab.Celebrity>(
-		"https://v1.hitokoto.cn/?c=d&c=e&c=h&c=i&c=k",
-	);
+	const res = await axios.get<OneTab.Celebrity>("https://v1.hitokoto.cn/?c=d&c=e&c=h&c=i&c=k");
 
 	return res.data;
 };
 
 // 获取壁纸分类
 export const getWallpaperGroup = async () => {
-	const res = await api.get<ResponseCommon<OneTab.WallpaperGroup[]>>(
-		"wallpaper-tags",
-		{
-			params: {
-				populate: "cover",
-			},
+	const res = await api.get<ResponseCommon<OneTab.WallpaperGroup[]>>("wallpaper-tags", {
+		params: {
+			populate: "cover",
 		},
-	);
+	});
 
 	return res.data.data;
 };
@@ -60,6 +52,13 @@ export const getWallpaperList = async (params: { tag: string }) => {
 			populate: "photo",
 		},
 	});
+
+	return res.data;
+};
+
+// 获取 APP 分类
+export const getAppGroup = async () => {
+	const res = await api.get<ResponseCommon<{ id: string; name: string; tag: string }[]>>("app-tags");
 
 	return res.data;
 };
--- packages/web-app/src/components/Modals/AddApp/index.tsx
+++ packages/web-app/src/components/Modals/AddApp/index.tsx
@@ -4,23 +4,29 @@ import { WindowControl } from "@/components/WindowControl";
 import { events } from "@/events";
 import { Dialog } from "@onetab/ui";
 import { useMount } from "ahooks";
+import { useRequest } from "ahooks";
 import { useState } from "react";
 import type { FC } from "react";
+
+import { getAppGroup } from "@/api";
 import { AppCard } from "./AppCard";
 
-const items: SideBarItem[] = [
+const DEFAULT_ITEMS: SideBarItem[] = [
 	{
-		key: "all",
-		title: "最受欢迎",
-	},
-	{
-		key: "shop",
-		title: "购物",
-		icon: "gouwu",
+		key: "",
+		title: "全部",
 	},
 ];
 
 export const AddAppModal: FC = () => {
+	const { data = [] } = useRequest(() =>
+		getAppGroup().then((res) =>
+			res.data.map<SideBarItem>((it) => ({
+				key: it.tag,
+				title: it.name,
+			})),
+		),
+	);
 	const [open, setOpen] = useState(false);
 	const [tabKey, setTabKey] = useState("all");
 
@@ -36,9 +42,9 @@ export const AddAppModal: FC = () => {
 			onOpenChange={setOpen}
 			mask={false}
 			closable={false}
-			className="w-[1024px] max-w-[calc(100vw-40px)] overflow-hidden h-[640px] max-h-[calc(100vh-40px)] bg-color-m1 bg-opacity-70 backdrop-blur-[40px] rounded-[12px] p-0"
+			className="w-[1024px] max-w-[calc(100vw-40px)] h-[640px] max-h-[calc(100vh-40px)] bg-color-m1 bg-opacity-70 backdrop-blur-[40px] rounded-[12px] p-0 overflow-hidden"
 		>
-			<div className="flex flex-col">
+			<div className="flex flex-col h-full overflow-hidden">
 				<div className="header flex h-[64px] items-center justify-between border-b-[1px] border-color-black border-opacity-5 px-[20px] dark:border-opacity-40 mb:!h-auto mb:flex-col-reverse mb:items-end mb:!p-[20px]">
 					<WindowControl onClose={() => handleClose()} />
 
@@ -50,12 +56,12 @@ export const AddAppModal: FC = () => {
 					</div>
 				</div>
 
-				<div className="flex-1 flex">
+				<div className="flex-1 flex overflow-hidden">
 					<Sidebar
-						items={items}
+						items={[...DEFAULT_ITEMS, ...data]}
 						activeKey={tabKey}
 						onItemClick={setTabKey}
-						className="w-60"
+						className="w-60 overflow-y-auto"
 					/>
 
 					<div className="flex-1 grid grid-cols-4 gap-4 px-[16px] pt-[32px] pb-[40px]">
