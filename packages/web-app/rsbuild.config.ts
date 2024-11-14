9bab2ba38c0bd977a285ca1884a637101dc9790b
jfbi <jfbi@iflytek.com>
2024-11-14 21:13:27 +0800

    feat(web-app): add svg symbol

--- packages/web-app/rsbuild.config.ts
+++ packages/web-app/rsbuild.config.ts
@@ -1,22 +1,30 @@
+import fs from "node:fs";
+import path from "node:path";
 import { defineConfig } from "@rsbuild/core";
 import { pluginImageCompress } from "@rsbuild/plugin-image-compress";
 import { pluginReact } from "@rsbuild/plugin-react";
 
-export default defineConfig(({ env }) => {
-  const isDev = env === "development";
+const svgSymbolFilePath = path.join(__dirname, "./src/assets/icons/iconfont-symbol.svg");
+const svgSymbolData = fs.readFileSync(svgSymbolFilePath, "utf8");
 
-  return {
-    plugins: [pluginReact(), pluginImageCompress()],
-    html: {
-      template: "./index.html",
-    },
-    source: {
-      entry: {
-        index: "./src/main.tsx",
-      },
-    },
-    output: {
-      assetPrefix: isDev ? "/" : "/one-tab/",
-    },
-  };
+export default defineConfig(({ env }) => {
+	const isDev = env === "development";
+
+	return {
+		plugins: [pluginReact(), pluginImageCompress()],
+		html: {
+			template: "./index.html",
+			templateParameters: {
+				svgSymbol: svgSymbolData,
+			},
+		},
+		source: {
+			entry: {
+				index: "./src/main.tsx",
+			},
+		},
+		output: {
+			assetPrefix: isDev ? "/" : "/one-tab/",
+		},
+	};
 });
